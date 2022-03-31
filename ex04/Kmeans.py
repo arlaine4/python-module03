2b5968f8d19a4cc565aab66ca77cf0ef1591bd47
Arthur LAINE <arlaine@student.42.fr>
2022-03-31 13:05:26 +0200

    undergoing chantier in fit method, still trying to figure out how to measure accuracy of the clusters

--- ex04/Kmeans.py
+++ ex04/Kmeans.py
@@ -2,9 +2,10 @@ import pandas as pd
 import argparse
 import sys
 import numpy as np
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeansi, DBSCAN
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def parse_arguments():
@@ -30,18 +31,33 @@ class KmeansClustering:
         self.n_centroid = n_centroid
         self.max_iter = max_iter
         self.centroids = []
-        self.model = KMeans(n_clusters=self.n_centroid,
-                         max_iter=self.max_iter,
-                         n_init=42,
-                         init='random')
+        self.model = None
 
     def fit(self, X):
-        self.model.fit(X)
-        print('Cluster centers positions :\n', self.model.cluster_centers_)
+        save_centroids = []
+        for i in range(1, 20):
+            model = KMeans(n_clusters=self.n_centroid,
+                             max_iter=self.max_iter,
+                             init='random',
+                             algorithm='elkan')
+            model.fit(X)
+            save_centroids.append(model.cluster_centers_)
+
+        print('\n\033[37;1;4mCluster centers positions\033[0m :\n\n', self.model.cluster_centers_)
+        self.centroids = self.model.cluster_centers_
 
     def predict(self, X):
         y_hat = self.model.predict(X)
-        print('Predictions :\n', y_hat)
+        print('\n\n\033[37;1;4mPredictions\033[0m :\n\n', y_hat)
+        y_hat = list(y_hat)
+        print(f'\n\033[37;1;4mCentroid 0\033[0m : {y_hat.count(0)} / {len(y_hat)}'
+              f' --> {round(y_hat.count(0) / len(y_hat) * 100, 2)}%')
+        print(f'\033[37;1;4mCentroid 1\033[0m : {y_hat.count(1)} / {len(y_hat)}'
+              f' --> {round(y_hat.count(1) / len(y_hat) * 100, 2)}%')
+        print(f'\033[37;1;4mCentroid 2\033[0m : {y_hat.count(2)} / {len(y_hat)}'
+              f' --> {round(y_hat.count(2) / len(y_hat) * 100, 2)}%')
+        print(f'\033[37;1;4mCentroid 3\033[0m : {y_hat.count(3)} / {len(y_hat)}'
+              f' --> {round(y_hat.count(3) / len(y_hat) * 100, 2)}%', end='\n\n')
         return y_hat
 
 
@@ -49,6 +65,7 @@ class KmeansClustering:
         fig = plt.figure(figsize=(10, 10))
         ax = fig.add_subplot(111, projection='3d')
         ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=y_hat)
+        ax.scatter(self.centroids[:, 0], self.centroids[:, 1], self.centroids[:, 2], s=50, c='black')
         ax.set_xlabel('Height')
         ax.set_ylabel('Weight')
         ax.set_zlabel('Bone density')
@@ -68,4 +85,4 @@ if __name__ == "__main__":
     model = KmeansClustering(option.max_iter, option.ncentroid)
     model.fit(X)
     y_hat = model.predict(X)
-    model.plot_clusters_3D(X, y_hat)
+    #model.plot_clusters_3D(X, y_hat)
