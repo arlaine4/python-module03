5b84a873b95be982268e28ace3d6b0d9122f1153
Arthur LAINE <arlaine@student.42.fr>
2022-04-01 11:44:58 +0200

    minor fixes

--- ex02/ScrapBooker.py
+++ ex02/ScrapBooker.py
@@ -8,11 +8,14 @@ class ScrapBooker:
         if not isinstance(array, np.ndarray) or not isinstance(dim, tuple)\
                 or not isinstance(position, tuple):
             return None
-        if type(dim[0]) is not int or type(dim[1]) is not int or len(dim) != 2:
-            return None
-        if type(position[0]) is not int or type(position[1]) is not int or len(position) != 2:
-            return None
-        if position[0] + dim[0] > array.shape[0] or position[1] + dim[1] > array.shape[1]:
+        try:
+            if type(dim[0]) is not int or type(dim[1]) is not int or len(dim) != 2:
+                return None
+            if type(position[0]) is not int or type(position[1]) is not int or len(position) != 2:
+                return None
+            if position[0] + dim[0] > array.shape[0] or position[1] + dim[1] > array.shape[1]:
+                return None
+        except (IndexError, TypeError):
             return None
         return array[position[0]:position[0] + dim[0], position[1]: position[1] + dim[1]]
 
@@ -23,7 +26,7 @@ class ScrapBooker:
             return None
         if n <= 0 or not 0 <= axis <= 1:
             return None
-        if axis == 0 and n >= array.shape[0] or (axis == 1 and n >= array.shape[1]):
+        if len(array.shape) != 2 or axis == 0 and n >= array.shape[0] or (axis == 1 and n >= array.shape[1]):
             return None
         axis = 0 if axis == 1 else 1
         to_delete = [i - 1 for i in range(array.shape[axis] + 1) if i != 0 and i % n == 0]
